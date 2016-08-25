428f728675a840a265df09b43215541c9f88d72d
nathanfaucett <nathanfaucett@gmail.com>
2016-08-25 14:00:24 -0500

    first commit

---
+++ Histogram.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Histogram {
+	private static Scanner _scanner;
+	private static Map<String, Integer> _distribution;
+	private static int _max = 100;
+
+	static String getCmd() {
+		if (!_scanner.hasNextShort()) {
+			String cmd = _scanner.nextLine();
+			return cmd;
+		} else {
+			return null;
+		}
+	}
+
+	static int getShort() {
+		if (_scanner.hasNextShort()) {
+			int number = _scanner.nextShort();
+
+			if (number > 0 && number <= _max) {
+				return number;
+			} else {
+				System.out.println("Enter a Valid Integer between 1 and " + _max + ": ");
+				return Histogram.getShort();
+			}
+		} else {
+			System.out.println("Enter a Valid Integer between 1 and " + _max + ": ");
+			_scanner.next();
+			return Histogram.getShort();
+		}
+	}
+
+	private static void addNumber(int number) {
+		String string = Integer.toString(number);
+		int first = Character.getNumericValue(string.charAt(string.length() - 1));
+		int second = 0;
+
+		if (number > 10) {
+			second = Character.getNumericValue(string.charAt(string.length() - 2));
+		}
+
+		String min = second == 0 ? "01" : second + "1";
+		String max = second == 0 ? "10" : (second + 1) + "0";
+		String range = min + "-" + max;
+
+		if (_distribution.containsKey(range)) {
+			_distribution.put(range, _distribution.get(range) + 1);
+		} else {
+			_distribution.put(range, 1);
+		}
+	}
+
+	private static String printAsterisk(int count) {
+		String asterisk = "";
+
+		while (count-- > 0) {
+			asterisk += '*';
+		}
+
+		return asterisk;
+	}
+
+	private static void printTable() {
+		for (Map.Entry<String, Integer> entry : _distribution.entrySet()) {
+            System.out.println(entry.getKey() + " | " + Histogram.printAsterisk(entry.getValue()));
+		}
+	}
+
+    public static void main(String[] args) {
+        _scanner = new Scanner(System.in);
+        _distribution = new HashMap<String, Integer>();
+
+		System.out.println("Enter Integer between 1 and " + _max + ": ");
+
+		while (true) {
+			String cmd = Histogram.getCmd();
+
+			if (cmd != null) {
+				if (cmd.equals("q") || cmd.equals("Q") || cmd.equals("quit") || cmd.equals("QUIT")) {
+					Histogram.printTable();
+				}
+			} else {
+				int number = Histogram.getShort();
+				Histogram.addNumber(number);
+			}
+		}
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# CS2331-Histogram
+CS2331 Histogram
+
+
+PP 7 Design and implement an application that creates a histogram that allows you to visually inspect the frequency distribution of a set of values. The program should  read in an arbitrary number of integers that are in the range of 1 to 100 inclusive; then produce a chart similar to the one below that indicates how many input values fell in the range 1 to 10, 11 to 20, and so on. Print an asterisk for each value entered.
+
+```bash
+01-10     | ****
+11-20     | **
+21-30     | *****************************************
+31-40     |  
+41-50     | ***
+51-60     | *********
+61-70     | **
+71-80     | ******
+81-90     | *********
+91-100    | *************
+```
